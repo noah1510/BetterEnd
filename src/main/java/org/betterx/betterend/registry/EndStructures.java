bddfa09b12be6d86f94abf6d3c3e5c4b41195325
Frank <developer@ambertation.de>
2022-06-08 01:26:32 +0200

    Disabled REI as it crashes with release 1.19

--- src/main/java/org/betterx/betterend/registry/EndStructures.java
+++ src/main/java/org/betterx/betterend/registry/EndStructures.java
@@ -7,8 +7,9 @@ import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
 import net.minecraft.world.level.levelgen.structure.pieces.StructurePieceType;
 
+import org.betterx.bclib.api.v2.levelgen.structures.BCLStructure;
+import org.betterx.bclib.api.v2.levelgen.structures.BCLStructureBuilder;
 import org.betterx.bclib.api.v2.tag.TagAPI;
-import org.betterx.bclib.world.structures.BCLStructure;
 import org.betterx.betterend.BetterEnd;
 import org.betterx.betterend.world.structures.features.*;
 import org.betterx.betterend.world.structures.piece.*;
@@ -22,55 +23,44 @@ public class EndStructures {
                                                                              PaintedMountainPiece::new);
     public static final StructurePieceType NBT_PIECE = register("nbt_piece", NBTPiece::new);
 
-    public static final BCLStructure<GiantMossyGlowshroomStructure> GIANT_MOSSY_GLOWSHROOM = new BCLStructure<>(
-            BetterEnd.makeID("giant_mossy_glowshroom"),
-            GiantMossyGlowshroomStructure::new,
-            Decoration.SURFACE_STRUCTURES,
-            16,
-            8
-    );
-    public static final BCLStructure<MegaLakeStructure> MEGALAKE = new BCLStructure<>(
-            BetterEnd.makeID("megalake"),
-            MegaLakeStructure::new,
-            Decoration.RAW_GENERATION,
-            4,
-            1
-    );
-    public static final BCLStructure<MegaLakeSmallStructure> MEGALAKE_SMALL = new BCLStructure<>(
-            BetterEnd.makeID("megalake_small"),
-            MegaLakeSmallStructure::new,
-            Decoration.RAW_GENERATION,
-            4,
-            1
-    );
-    public static final BCLStructure<MountainStructure> MOUNTAIN = new BCLStructure<>(
-            BetterEnd.makeID("mountain"),
-            MountainStructure::new,
-            Decoration.RAW_GENERATION,
-            3,
-            2
-    );
-    public static final BCLStructure<PaintedMountainStructure> PAINTED_MOUNTAIN = new BCLStructure<>(
-            BetterEnd.makeID("painted_mountain"),
-            PaintedMountainStructure::new,
-            Decoration.RAW_GENERATION,
-            3,
-            2
-    );
-    public static final BCLStructure<EternalPortalStructure> ETERNAL_PORTAL = new BCLStructure<>(
-            BetterEnd.makeID("eternal_portal"),
-            EternalPortalStructure::new,
-            Decoration.SURFACE_STRUCTURES,
-            16,
-            6
-    );
-    public static final BCLStructure<GiantIceStarStructure> GIANT_ICE_STAR = new BCLStructure<>(
-            BetterEnd.makeID("giant_ice_star"),
-            GiantIceStarStructure::new,
-            Decoration.SURFACE_STRUCTURES,
-            16,
-            8
-    );
+    public static final BCLStructure<GiantMossyGlowshroomStructure> GIANT_MOSSY_GLOWSHROOM = BCLStructureBuilder
+                    .start(BetterEnd.makeID("giant_mossy_glowshroom"), GiantMossyGlowshroomStructure::new)
+                    .step(Decoration.SURFACE_STRUCTURES)
+                    .randomPlacement(16, 8)
+                    .build();
+
+    public static final BCLStructure<MegaLakeStructure> MEGALAKE = BCLStructureBuilder
+            .start(BetterEnd.makeID("megalake"), MegaLakeStructure::new)
+            .step(Decoration.RAW_GENERATION)
+            .randomPlacement(4, 1)
+            .build();
+
+    public static final BCLStructure<MegaLakeSmallStructure> MEGALAKE_SMALL = BCLStructureBuilder
+            .start(BetterEnd.makeID("megalake_small"), MegaLakeSmallStructure::new)
+            .step(Decoration.RAW_GENERATION)
+            .randomPlacement(4, 1)
+            .build();
+
+    public static final BCLStructure<MountainStructure> MOUNTAIN = BCLStructureBuilder
+            .start(BetterEnd.makeID("mountain"), MountainStructure::new)
+            .step(Decoration.RAW_GENERATION)
+            .randomPlacement(3, 2)
+            .build();
+    public static final BCLStructure<PaintedMountainStructure> PAINTED_MOUNTAIN = BCLStructureBuilder
+            .start(BetterEnd.makeID("painted_mountain"), PaintedMountainStructure::new)
+            .step(Decoration.RAW_GENERATION)
+            .randomPlacement(3, 2)
+            .build();
+    public static final BCLStructure<EternalPortalStructure> ETERNAL_PORTAL = BCLStructureBuilder
+            .start(BetterEnd.makeID("eternal_portal"), EternalPortalStructure::new)
+            .step(Decoration.RAW_GENERATION)
+            .randomPlacement(16, 6)
+            .build();
+    public static final BCLStructure<GiantIceStarStructure> GIANT_ICE_STAR = BCLStructureBuilder
+            .start(BetterEnd.makeID("giant_ice_star"), GiantIceStarStructure::new)
+            .step(Decoration.SURFACE_STRUCTURES)
+            .randomPlacement(16, 8)
+            .build();
 
     public static void register() {
     }
