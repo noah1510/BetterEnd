5cbccb1387a458a7f325075a39c1bde8516f9a0f
Frank <developer@ambertation.de>
2022-07-10 00:26:49 +0200

    Removed promenade debug logging

--- src/main/java/org/betterx/betterend/mixin/common/MappedRegistryMixin.java
+++ src/main/java/org/betterx/betterend/mixin/common/MappedRegistryMixin.java
@@ -1,7 +1,5 @@
 package org.betterx.betterend.mixin.common;
 
-import com.mojang.serialization.Lifecycle;
-import net.minecraft.core.Holder;
 import net.minecraft.core.MappedRegistry;
 import net.minecraft.resources.ResourceKey;
 
@@ -9,7 +7,6 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MappedRegistry.class)
 public class MappedRegistryMixin<T> {
@@ -19,35 +16,35 @@ public class MappedRegistryMixin<T> {
         //info.cancel();
     }
 
-    @Inject(method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;Z)Lnet/minecraft/core/Holder;", at = @At("HEAD"))
-    private void be_debugDummy(
-            int i,
-            ResourceKey<T> resourceKey,
-            T object,
-            Lifecycle lifecycle,
-            boolean bl,
-            CallbackInfoReturnable<Holder<T>> cir
-    ) {
-        if (resourceKey.location().getPath().equals("dark_amaranth_forest")) {
-            System.out.println("Promenade registers: " + resourceKey);
-        }
-    }
-
-    @Inject(method = "getOrCreateHolderOrThrow", at = @At("HEAD"))
-    private void be_debugDummy2(
-            ResourceKey<T> resourceKey, CallbackInfoReturnable<Holder<T>> cir
-    ) {
-        if (resourceKey.location().getPath().equals("dark_amaranth_forest")) {
-            System.out.println("Promenade registers: " + resourceKey);
-        }
-    }
-
-    @Inject(method = "getOrCreateHolder", at = @At("HEAD"))
-    private void be_debugDummy3(
-            ResourceKey<T> resourceKey, CallbackInfoReturnable<Holder<T>> cir
-    ) {
-        if (resourceKey.location().getPath().equals("dark_amaranth_forest")) {
-            System.out.println("Promenade registers: " + resourceKey);
-        }
-    }
+//    @Inject(method = "registerMapping(ILnet/minecraft/resources/ResourceKey;Ljava/lang/Object;Lcom/mojang/serialization/Lifecycle;Z)Lnet/minecraft/core/Holder;", at = @At("HEAD"))
+//    private void be_debugDummy(
+//            int i,
+//            ResourceKey<T> resourceKey,
+//            T object,
+//            Lifecycle lifecycle,
+//            boolean bl,
+//            CallbackInfoReturnable<Holder<T>> cir
+//    ) {
+//        if (resourceKey.location().getPath().equals("dark_amaranth_forest")) {
+//            System.out.println("Promenade registers: " + resourceKey);
+//        }
+//    }
+//
+//    @Inject(method = "getOrCreateHolderOrThrow", at = @At("HEAD"))
+//    private void be_debugDummy2(
+//            ResourceKey<T> resourceKey, CallbackInfoReturnable<Holder<T>> cir
+//    ) {
+//        if (resourceKey.location().getPath().equals("dark_amaranth_forest")) {
+//            System.out.println("Promenade registers: " + resourceKey);
+//        }
+//    }
+//
+//    @Inject(method = "getOrCreateHolder", at = @At("HEAD"))
+//    private void be_debugDummy3(
+//            ResourceKey<T> resourceKey, CallbackInfoReturnable<Holder<T>> cir
+//    ) {
+//        if (resourceKey.location().getPath().equals("dark_amaranth_forest")) {
+//            System.out.println("Promenade registers: " + resourceKey);
+//        }
+//    }
 }
