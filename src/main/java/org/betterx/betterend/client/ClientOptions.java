01d4d0851ad9c552abbb9eb67e033ffbe495f1d7
Frank <developer@ambertation.de>
2022-12-10 17:46:19 +0100

    [Fix] `useFogDensity` is no longer used (#119)

--- src/main/java/org/betterx/betterend/client/ClientOptions.java
+++ src/main/java/org/betterx/betterend/client/ClientOptions.java
@@ -4,13 +4,11 @@ import org.betterx.betterend.config.Configs;
 
 public class ClientOptions {
     private static boolean customSky;
-    private static boolean useFogDensity;
     private static boolean blendBiomeMusic;
     private static boolean sulfurWaterColor;
 
     public static void init() {
         customSky = Configs.CLENT_CONFIG.getBooleanRoot("customSky", true);
-        useFogDensity = Configs.CLENT_CONFIG.getBooleanRoot("useFogDensity", true);
         blendBiomeMusic = Configs.CLENT_CONFIG.getBooleanRoot("blendBiomeMusic", true);
         sulfurWaterColor = Configs.CLENT_CONFIG.getBooleanRoot("sulfurWaterColor", true);
         Configs.CLENT_CONFIG.saveChanges();
@@ -22,14 +20,6 @@ public class ClientOptions {
 
     public static void setCustomSky(boolean customSky) {
         ClientOptions.customSky = customSky;
-    }
-
-    public static boolean useFogDensity() {
-        return useFogDensity;
-    }
-
-    public static void setUseFogDensity(boolean useFogDensity) {
-        ClientOptions.useFogDensity = useFogDensity;
     }
 
     public static boolean blendBiomeMusic() {
