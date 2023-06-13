af680dcfdc6d41d4731e3011d894a1dd6478c57a
Frank <developer@ambertation.de>
2023-06-13 22:39:29 +0200

    [Change] Force Generation of Tag `c:nether_mycelium`

--- src/main/java/org/betterx/datagen/betterend/EndBlockTagDataProvider.java
+++ src/main/java/org/betterx/datagen/betterend/EndBlockTagDataProvider.java
@@ -2,6 +2,7 @@ package org.betterx.datagen.betterend;
 
 import org.betterx.bclib.api.v3.datagen.TagDataProvider;
 import org.betterx.betterend.BetterEnd;
+import org.betterx.worlds.together.tag.v3.CommonBlockTags;
 import org.betterx.worlds.together.tag.v3.TagManager;
 
 import net.minecraft.core.HolderLookup;
@@ -10,6 +11,7 @@ import net.minecraft.world.level.block.Block;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 public class EndBlockTagDataProvider extends TagDataProvider<Block> {
@@ -18,6 +20,12 @@ public class EndBlockTagDataProvider extends TagDataProvider<Block> {
             FabricDataOutput output,
             CompletableFuture<HolderLookup.Provider> registriesFuture
     ) {
-        super(TagManager.BLOCKS, List.of("minecraft", BetterEnd.MOD_ID, "c"), output, registriesFuture);
+        super(
+                TagManager.BLOCKS,
+                List.of("minecraft", BetterEnd.MOD_ID, "c"),
+                Set.of(CommonBlockTags.NETHER_MYCELIUM),
+                output,
+                registriesFuture
+        );
     }
 }
